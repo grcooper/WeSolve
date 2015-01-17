20395c8e022a4f25e40f06c2a50b3788c593f15c
thadeaus <graham.cooper33@gmail.com>
2015-01-17 09:21:55 -0500

    log in and you play

--- wesolve.js
+++ wesolve.js
@@ -1,18 +1,23 @@
 if (Meteor.isClient) {
   //This code is only run on the client
-  function hasLogged(){
-    Template.body.noUser = false;
-  }
   
   //Config accounts
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
+  });
+  //inBody functions
+
+  //Landing Page Functions
+  Template.inBody.events({
+    'click button': function(){
+      //Switch to Play view
+      Session.set("hasUser", true);
+    }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
-    Template.body.noUser = true;
   });
 }
